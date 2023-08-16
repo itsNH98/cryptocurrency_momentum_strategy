fb3eb5b0ebe06317bf7e55f1c53a1d32b24ba523
NH98 <32113063+itsNH98@users.noreply.github.com>
2023-08-15 20:32:40 -0400

    Std commit

--- code/weekly_top_attention.R
+++ code/weekly_top_attention.R
@@ -6,8 +6,8 @@ library(dplyr)
 library(zoo)
 
 # CRYPTO RETRIEVAL -------------------------------------------------
-keywords_long <- c('Shiba Inu', 'Uniswap', 'Binance')
-keywords_short <- c('XRP', 'MATIC', 'Litecoin')
+keywords_long <- c('Shiba Inu', 'Solana', 'Bitcoin -cash -gold')
+keywords_short <- c('XLM', 'XRP', 'Litecoin')
 all_cryptos <- c(keywords_long, keywords_short)
 
 # Function to get interest data for a keyword set and pivot the results
